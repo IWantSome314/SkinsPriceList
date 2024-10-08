77f88a08a1dc926f978421ed9442d3fe68cd6270
MyOnlyFriend <85770767+IWantSome314@users.noreply.github.com>
2024-10-08 14:03:49 +0100

    Update skinfinder.py

--- skinfinder.py
+++ skinfinder.py
@@ -10,4 +10,3 @@ else:
 gunSelected= requests.get(f"https://steamcommunity.com/market/priceoverview/?appid=730&currency=1&market_hash_name={gun}%20%7C%20{skinname}%20({wear})")
 print(f"Here is your {skinnameuser} {gun} in {wearuser}")
 print(gunSelected.text)
-lexi
